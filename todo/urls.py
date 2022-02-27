3062a5201a75c1bc830e42b368388618ed1bd412
Anton Mindlin <anton.mindlin@gmail.com>
2022-02-28 00:40:45 +0100

    Lesson_1. Base API for User model & admin

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+import usersapp.views as usersapp
+
+router = DefaultRouter()
+router.register('usersapp', usersapp.UserModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
 ]
