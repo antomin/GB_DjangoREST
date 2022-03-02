f9a575e3778d16504ff5ea46c410951eee9a2de2
Anton Mindlin <anton.mindlin@gmail.com>
2022-03-02 16:29:03 +0100

    Lesson_2. Components local start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserList from "./components/Users";
 
 class App extends React.Component {
     constructor(props) {
@@ -10,13 +11,37 @@ class App extends React.Component {
         };
     }
 
+    componentDidMount() {
+        const users = [
+            {
+                "username": "archy",
+                "first_name": "Artur",
+                "last_name": "Arturov",
+                "email": "archy@local.host",
+            },
+            {
+                "username": "olya",
+                "first_name": "Olga",
+                "last_name": "Olgeeva",
+                "email": "olya@local.host",
+            },
+        ];
+
+        this.setState(
+            {
+                'users': users
+            }
+        )
+    }
+
     render() {
         return (
             <div>
-                Hello world!
+                <UserList users={this.state.users} />
             </div>
         )
     }
+
 }
 
-export default App;
+    export default App;
