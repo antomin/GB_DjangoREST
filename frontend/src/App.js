01a5edf091b9b1d671c46ed29f415df80399ed42
Anton Mindlin <anton.mindlin@gmail.com>
2022-03-03 00:59:49 +0100

    Lesson_2. Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-import UserList from "./components/Users";
 import axios from "axios";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import UserList from "./components/Users";
 
 class App extends React.Component {
     constructor(props) {
@@ -14,21 +14,6 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        // const users = [
-        //     {
-        //         "username": "archy",
-        //         "first_name": "Artur",
-        //         "last_name": "Arturov",
-        //         "email": "archy@local.host",
-        //     },
-        //     {
-        //         "username": "olya",
-        //         "first_name": "Olga",
-        //         "last_name": "Olgeeva",
-        //         "email": "olya@local.host",
-        //     },
-        // ];
-
         axios.get('http://127.0.0.1:8000/api/usersapp/').then(response => {
             const users = response.data;
             this.setState({'users': users});
@@ -42,6 +27,7 @@ class App extends React.Component {
             <div>
                 <Header />
                 <UserList users={this.state.users}/>
+                <Footer />
             </div>
         )
     }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="footer bg-dark fixed-bottom">
+            <div className="container">
+                <div className="text-muted py-3 text-center">Anton Mindlin</div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
