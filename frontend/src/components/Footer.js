371890fd6e1c82dba9440ccad6c7108cbe978e98
Anton Mindlin <anton.mindlin@gmail.com>
2022-03-03 01:13:03 +0100

    Lesson_2. Copyright year

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,8 +1,13 @@
+const NowYear = () => {
+    const CurrentDate = new Date();
+    return CurrentDate.getFullYear();
+};
+
 const Footer = () => {
     return (
         <footer className="footer bg-dark fixed-bottom">
             <div className="container">
-                <div className="text-muted py-3 text-center">Anton Mindlin</div>
+                <div className="text-muted py-3 text-center"><NowYear/> &copy; Anton Mindlin</div>
             </div>
         </footer>
     );
