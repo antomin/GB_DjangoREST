c23934ab993f8a6135e6072c71fbe9d6fdfd98cb
Anton Mindlin <37115351+antomin@users.noreply.github.com>
2022-03-04 15:36:22 +0100

    Lesson 2 (#2)
    * Lesson_2. Ready

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+const NowYear = () => {
+    const CurrentDate = new Date();
+    return CurrentDate.getFullYear();
+};
+
+const Footer = () => {
+    return (
+        <footer className="footer bg-dark fixed-bottom">
+            <div className="container">
+                <div className="text-muted py-3 text-center"><NowYear/> &copy; Anton Mindlin</div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
