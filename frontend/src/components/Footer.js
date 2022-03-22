c08624a6c5e6280a1216c340711d7b66e1b16670
Anton Mindlin <anton.mindlin@gmail.com>
2022-03-22 11:25:29 +0100

    Lesson_2. ready

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,9 @@ const Footer = () => {
     return (
         <footer className="footer bg-dark fixed-bottom">
             <div className="container">
-                <div className="text-muted py-3 text-center"><NowYear/> &copy; Anton Mindlin</div>
+                <div className="text-muted py-3 text-center">
+                    <NowYear/> &copy; Anton Mindlin
+                </div>
             </div>
         </footer>
     );
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,17 +2,17 @@ const Header = () => {
     return (
         <nav className="navbar navbar-dark bg-dark pb-0 mb-4 text-white">
             <div className="container">
-            <ul className="nav nav-tabs nav-dark">
-                <li className="nav-item">
-                    <a className="nav-link active" href="#">Пользователи</a>
-                </li>
-                <li className="nav-item navbar-dark">
-                    <a className="nav-link text-light" href="#">Проекты</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link text-light" href="#">ToDo</a>
-                </li>
-            </ul>
+                <ul className="nav nav-tabs nav-dark">
+                    <li className="nav-item">
+                        <a className="nav-link active" href="#">Пользователи</a>
+                    </li>
+                    <li className="nav-item navbar-dark">
+                        <a className="nav-link text-light" href="#">Проекты</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link text-light" href="#">ToDo</a>
+                    </li>
+                </ul>
             </div>
         </nav>
     );
