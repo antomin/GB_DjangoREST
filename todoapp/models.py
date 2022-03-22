0779066d411e99ee5365c303f0f3c33308f9530c
Anton Mindlin <anton.mindlin@gmail.com>
2022-03-22 21:39:54 +0100

    Lesson_3. djangorestframework-camel-case instaled

--- todoapp/models.py
+++ todoapp/models.py
@@ -16,6 +16,6 @@ class ToDo(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     text = models.TextField()
     author = models.ForeignKey(User, on_delete=models.PROTECT)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
