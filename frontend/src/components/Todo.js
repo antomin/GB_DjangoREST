1e47993f0d079023bec0d9de552e0bbcaeed65d4
Anton Mindlin <anton.mindlin@gmail.com>
2022-03-30 11:36:02 +0200

    Todo component

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -1,2 +1,34 @@
 import React from "react";
 
+const TodoItem = ({task}) => {
+    return (
+        <tr>
+            <td>task.project</td>
+            <td>task.author</td>
+            <td>task.createdAt</td>
+            <td>task.updatedAt</td>
+            <td>task.isActive</td>
+        </tr>
+    );
+};
+
+const TodoList = ({tasks}) => {
+    return (
+        <div className="container">
+            <table className="table table-striped">
+                <thead>
+                <th>Проект</th>
+                <th>Автор</th>
+                <th>Время создания</th>
+                <th>Последнее обновление</th>
+                <th>Активна</th>
+                </thead>
+                <tbody>
+                {tasks.map((task) => <TodoItem task={task}/>)}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default TodoList;
